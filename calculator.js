357fcbe203f22b62e8c48d66a4deac657a312107
Timothy Bell <timothy.bell@ttu.edu>
2018-10-19 15:26:21 -0500

    Added jQuery package

--- calculator.js
+++ calculator.js
@@ -1,28 +1,28 @@
 $(document).ready(function() {
-  let display = $("#display")[0]
+  let display = $("#display")[0].innerHTML
 
   // Build an expression
   $(".expr").click(function(e) {
-    display.innerHTML += e.currentTarget.value
+    display += e.currentTarget.value
   })
 
   // Delete some numbers
   $("#del").click(function() {
-    display.innerHTML = display.innerHTML.substr(0, display.innerHTML.length-2)
+    display = display.substr(0, display.length-2)
   })
 
   // Clear all content in display
   $("#del").dblclick(function() {
-    display.innerHTML = ""
+    display = ""
   })
 
   // Evaluate an expression
   $("#calc").click(function() {
     try {
-      display.innerHTML = eval(display.innerHTML)
+      display = eval(display)
     } catch (e) {
       setTimeout(function () {
-        display.innerHTML = e
+        display = e
       }, 3*1000);
     }
   })
